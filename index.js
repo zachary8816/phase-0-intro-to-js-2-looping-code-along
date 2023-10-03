1e0f421e1d4c3f98f95b31c0164a5168df3ca1b2
zachary <zmh012004@gmail.com>
2023-10-02 23:34:43 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  const names = ["Alice", "Bob", "Carol"];
+  const event = "birthday party";
+  
+  const thankYouMessages = writeCards(names, event);
+  console.log(thankYouMessages);
+  assert.deepEqual(thankYouMessages, [
+    "Thank you, Guadalupe, for attending the surprise event.",
+    "Thank you, Ollie, for attending the surprise event.",
+    "Thank you, Aki, for attending the surprise event."
+  ]);
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
